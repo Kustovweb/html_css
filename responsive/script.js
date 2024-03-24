e2a549a6ffb8fdf7e504776acd2c39d974a2bff1
zk <kustovweb@gmail.com>
2024-03-24 19:45:51 +0700

    html_css_pro

--- responsive/script.js
+++ responsive/script.js
@@ -1,14 +1,25 @@
-var cart = document.querySelector('.cart');
 var badge = document.querySelector('.badge');
 var menu = document.querySelector('.menu');
 var menuVisible = document.querySelector('.menu__list');
+var addCart = document.querySelectorAll('[data-product-id]');
+var link = document.getElementsByClassName('breadcrumbs__link');
 var count = 0;
-cart.addEventListener("click", function() {
-   count += 1;
-   badge.innerHTML = count;
 
-});
-
-menu.addEventListener("click", function() {
+menu.addEventListener("click", function () {
    menuVisible.classList.toggle('active');
 });
+
+for (let i = 0; link.length; i++) {
+   if (link[i].href === window.location.href) {
+      link[i].classList.add('active__link');
+      break;
+   }
+}
+
+addCart.forEach(item => {
+   item.addEventListener("click", function () {
+      count += 1;
+      badge.innerHTML = count;
+
+   });
+})
